d588d62509f8bc6f98f4bd6c308ecc5689611cf8
Andrey Liapin <liapin@live.ru>
2016-05-13 22:47:36 +0300

    add controllers and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :projects do 
-    resources :tasks
+  resources :projects, except: :show do 
+    resources :tasks, except: :show
   end
 
   root 'projects#index'
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
