4c1236132834419355eec3a8b37ae3447387a767
Andrey Liapin <liapin@live.ru>
2016-08-12 20:39:19 +0300

    fast bugfix

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,8 +1,5 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-$(function(){
-	$('.edit_task input[type=checkbox]').click(function() {
-		$(this).submit();
-	});
-});
+var submitOnCheck = function(id){
+	$("#edit_task_"+id).submit();
+};
